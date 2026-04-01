d5ec0685e7576f5950dc6dd6fc3283ca17a47d6b
lifangzheng <1397118980@qq.com>
2026-04-02 00:24:07 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import { buildPlugins } from './vite/plugin'
-import { loadEnv } from 'vite'
-import buildConfig from './vite/config'
+import { defineConfig } from "vite";
+import { buildPlugins } from "./vite.conf/plugin";
+import { loadEnv } from "vite";
+import buildConfig from "./vite.conf/config";
 
 export default defineConfig((conf) => {
-  const { mode, command } = conf
-  const env = loadEnv(mode, process.cwd())
-  const isBuild = command === 'build'
+  const { mode, command } = conf;
+  const env = loadEnv(mode, process.cwd());
+  const isBuild = command === "build";
   return {
     ...buildConfig({ ...conf, isBuild, env }),
     plugins: buildPlugins({ mode, isBuild, env }),
-
-  }
-})
+  };
+});
